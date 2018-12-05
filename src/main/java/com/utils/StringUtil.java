1b3dc385be3d469e473cca9f61046287fba9d347
anthonylxd <lxdblues@gmail.com>
2018-12-06 00:05:16 +0800

    start

--- src/main/java/com/utils/StringUtil.java
+++ src/main/java/com/utils/StringUtil.java
@@ -1,4 +1,18 @@
 package com.utils;
 
 public class StringUtil {
+
+    /**
+     * 判断字符串是否为空
+     * 空返回false
+     * 非空返回true
+     * @param str
+     * @return
+     */
+    public static boolean notEmpty(String str) {
+        if(str != null && "".equals(str.trim())){
+            return true;
+        }
+        return false;
+    }
 }
